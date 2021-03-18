e9d8c897f11d6936a300ce5599a5f1dcc5cc2319
Alonso Isidoro Román <alonsoir@gmail.com>
2021-03-18 17:00:44 +0100

    Testing Adaptive Query Planner.

--- src/main/scala/part5lowlevel/RDDs.scala
+++ src/main/scala/part5lowlevel/RDDs.scala
@@ -12,6 +12,35 @@ object RDDs extends App {
     .config("spark.master", "local")
     .getOrCreate()
 
+  // Voy a probar a medir Adaptive Query Planner
+
+  import spark.implicits._
+  val simpleData = Seq(("James","Sales","NY",90000,34,10000),
+    ("Michael","Sales","NY",86000,56,20000),
+    ("Robert","Sales","CA",81000,30,23000),
+    ("Maria","Finance","CA",90000,24,23000),
+    ("Raman","Finance","CA",99000,40,24000),
+    ("Scott","Finance","NY",83000,36,19000),
+    ("Jen","Finance","NY",79000,53,15000),
+    ("Jeff","Marketing","CA",80000,25,18000),
+    ("Kumar","Marketing","NY",91000,50,21000)
+  )
+  val df = simpleData.toDF("employee_name","department","state","salary","age","bonus")
+
+  val df1=df.groupBy("department").count()
+  println(df1.rdd.getNumPartitions)
+  // 200 partitions!
+  spark.conf.set("spark.sql.adaptive.enabled",true)
+  spark.conf.set("spark.sql.adaptive.coalescePartitions.enabled",true)
+
+  val df2=df.groupBy("department").count()
+  println(df2.rdd.getNumPartitions)
+  // 1 partition
+
+  spark.conf.set("spark.sql.adaptive.skewJoin.enabled",true)
+  val df3=df.groupBy("department").count()
+  println(df3.rdd.getNumPartitions)
+  // 1 partition
   // the SparkContext is the entry point for low-level APIs, including RDDs
   val sc = spark.sparkContext
 
